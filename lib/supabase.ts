2f213f60a57b1732e7e492c68c4d73761262b3db
Ghost <ghostofprotocol@gmail.com>
2026-08-01 01:49:14 +0100

    Support Supabase publishable key environment

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,23 +1,19 @@
 import { createClient } from "@supabase/supabase-js";
 
-console.log(
-  "Supabase URL:",
-  process.env.NEXT_PUBLIC_SUPABASE_URL
-);
-
-console.log(
-  "Supabase Key:",
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
-
 const supabaseUrl =
-  process.env.NEXT_PUBLIC_SUPABASE_URL!;
+  process.env.NEXT_PUBLIC_SUPABASE_URL;
 
 const supabaseKey =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
-)
+console.log("Supabase URL:", supabaseUrl);
+console.log("Supabase Key exists:", !!supabaseKey);
+
+
+export const supabase =
+  createClient(
+    supabaseUrl!,
+    supabaseKey!
+  );
